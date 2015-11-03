8b80fa080f2f8f73e97d6e1872da1466826bcbbc
yusriy <yusriyp@gmail.com>
2015-11-03 22:25:19 +0800

    First commit of scripts.

---
+++ R/analyses_script.R
@@ -0,0 +1,10 @@
+#################################################
+# TITLE: NOCTURNAL URBAN TURBULENCE DATA ANALYSES
+#
+# AUTHOR: YUSRI YUSUP, PHD
+# DATE: 2015-11-03
+# 
+##################################################
+# Load the data
+secA <- read.csv('data/sectorA.csv',header=TRUE)
+secD <-read.csv('data/sectorD.csv',header=TRUE)
